5617ec79c58ad3685dba9ab6d04d55ae25aa36fa
Đồng Quốc Tranh <tranhdq@d-soft.com.vn>
2021-05-04 22:38:22 +0700

    update

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import sys
 from PyQt5 import QtWidgets
 from ui.src.controllers.controller import MainController
+from TestPage import Main
 import signal
 
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 
 app = QtWidgets.QApplication(sys.argv)
-window = MainController()
+# window = MainController()
+window = Main()
 window.show()
 sys.exit(app.exec_())
